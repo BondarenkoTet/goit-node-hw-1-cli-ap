76336e1eafb4f338692ffc38374ea410da68c6c0
BondarenkoTet <111590245+BondarenkoTet@users.noreply.github.com>
2023-06-05 15:56:52 +0300

    HW-01

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ async function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
     case "list":
         const allContacts = await contacts.listContacts();
-        console.table(allContacts);
-    break;
-
+        return console.table(allContacts);
     case "get":
         const oneContacts = await contacts.getContactById(id);
         return console.log(oneContacts);
-
     case "add":
         const newContact = await contacts.addContact({name, email, phone}); 
         return console.log(newContact);
-
     case "remove":
         const delContact = await contacts.removeContact(id);
         return console.log(delContact);
@@ -37,13 +33,3 @@ program.parse();
 const options = program.opts()
 invokeAction(options);
 
-//const arr = hideBin(process.argv);
-//const {argv} = yargs(arr);
-//invokeAction(argv);
-
-invokeAction({action: "list"})
-//invokeAction({action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw"})
-//invokeAction({action: "add", name: "Den", email: "mattis@nonenimMauris.net", phone: "(542) 451-56565"})
-//invokeAction({action: "remove", id:"05olLMgyVQdWRwgKfg5J6"})
-
-//console.log(process.argv);
---
+++ readme.md
@@ -0,0 +1,4 @@
+https://ibb.co/JBFJ69Z
+https://ibb.co/Fzv3wBy
+https://ibb.co/F397R8J
+https://ibb.co/K01Gd9z
